f783191a485f506937972f5a4eb15b5d43eb50e7
cocoball200 <cocoball200@gmail.com>
2023-08-22 15:42:04 +0900

    fix: allow http

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className}>
         <div className="main_layout">
           <div tabIndex="-1" className="main_container">
