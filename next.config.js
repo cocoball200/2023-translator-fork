c9b6cf8ddf47a561d3133b4531141df3c15daa85
cocoball200 <cocoball200@gmail.com>
2023-08-22 22:06:39 +0900

    refactor: unnecessary code

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        destination: "https://cs93.site/:path*",
-        source: "/a/:path*",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/assets/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
